0011e94013ca685840f996bf58862a17edf30a63
yaoxiaobing <xiaobing.yao@duobei.com>
2020-09-14 14:28:03 +0800

    feat: add --enable-symbol-hiding

--- build_libcurl_dist.sh
+++ build_libcurl_dist.sh
@@ -22,7 +22,7 @@ function build_for_arch() {
   export PATH="${DEVROOT}/usr/bin/:${PATH}"
   export CFLAGS="-DCURL_BUILD_IOS -arch ${ARCH} -pipe -Os -gdwarf-2 -isysroot ${SYSROOT} -miphoneos-version-min=${IPHONEOS_DEPLOYMENT_TARGET} -fembed-bitcode"
   export LDFLAGS="-arch ${ARCH} -isysroot ${SYSROOT}"
-  ./configure --disable-shared --without-ssl --with-secure-transport --without-zlib --enable-static --enable-ipv6  --host="${HOST}" --prefix=${PREFIX} && make -j8 && make install
+  ./configure --disable-shared --enable-symbol-hiding --without-ssl --with-secure-transport --without-zlib --enable-static --enable-ipv6  --host="${HOST}" --prefix=${PREFIX} && make -j8 && make install
 }
 
 
